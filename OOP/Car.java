0515e16b4c60b79eb80683c409eaa7d73f1d2a68
Sumit Chauhan <chauhansumit9009@gmail.com>
2024-02-24 17:47:17 +0530

    .

---
+++ OOP/Car.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class Car {
+    private int numberOfWheels;
+    private int seats;
+
+    Car(int numberOfWheels, int seats){
+        this.numberOfWheels= numberOfWheels;
+        this.seats = seats;
+    }
+
+    public void getNumberOfWheels(){
+        System.out.println(numberOfWheels);
+    }
+
+    public void getSeats(){
+        System.out.println(numberOfWheels);
+    }
+    public static void main(String[] args) {
+        Car obj = new Car(4, 4);
+        obj.getNumberOfWheels();
+        obj.getSeats();
+    }
+}
